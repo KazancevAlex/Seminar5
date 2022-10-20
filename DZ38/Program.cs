8cb3b2da8b39165cef8512fb07de0ae837f32a38
Alexandr <kazancevalex1987@gmail.com>
2022-10-20 21:49:22 +1200

    Домашнее задание 38

---
+++ DZ38/Program.cs
@@ -0,0 +1,28 @@
+﻿// Домашнее задание 38
+
+void InputArray(double[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = Math.Round(new Random().NextDouble() * 10, 2);
+}
+
+double CountEventNumbers(double[] array)
+{
+double minArray = array[0], maxArray = array[0];
+for (int i = 1; i < array.Length; i++) 
+{
+if (maxArray < array[i])
+maxArray = array[i];
+else if (minArray > array[i])
+minArray = array[i];
+}
+return maxArray - minArray;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Разница между максимальным и минимальным= {CountEventNumbers(array)}");
